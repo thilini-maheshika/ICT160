d50d8bd86430071c8bcc98b0875df102f16e86e9
ICT160 <kthilini1999@gmail.com>
2022-01-11 14:59:41 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+    class StudentParent{
+        public $stdName;
+        public $stdRegNum;
+
+            function __construct($name, $regnum) {
+                $this->stdName = $name;
+                $this->stdRegNum = $regnum;
+            }
+        
+    }
+
+
+?>
